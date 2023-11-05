07f0896fd24b0c6e868a21fb5a87f3d58b4dc0f9
David Santamaria <davidsantamaria1245@gmail.com>
2023-11-05 17:28:26 -0500

    add fen button, minor changes to wrapper for stockfish

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
+import { validateFen } from 'chess.js'
 
-import useStockfishEngine from './Engine.js';
+import Engine from './Engine.js';
 
 const DEPTH = 8;
 const FEN_POSITION = 
-    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    "rnbqkb1r/pp1p1ppp/2p2n2/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 2 4";
 
 export default function App() {
     const [game, setGame] = useState(new Chess());
-    const { evaluatePosition, stop, bestMove } = useStockfishEngine();
+    const [fenInput, setFenInput] = useState("");
+    const [evaluation, setEvaluation] = useState();
+    const { evaluatePosition, getEval, stop } = Engine();
 
     useEffect(()=> {
 
+        return ()=> {
+            stop();
+        }
     }, [game]);
 
     function onDrop(sourceSquare, targetSquare) {
@@ -25,20 +31,50 @@ export default function App() {
         try {
             if (cpy.move(move) != null) {
                 setGame(cpy);
-                return true;
-            } else {
-                return false;
             }
         } catch (error) {
             console.log(error);
+            return false;
         }
 
         evaluatePosition(game.fen());
+        return true;
+    }
+
+    function handleFenChange(event) {
+        setFenInput(event.target.value);
+    }
+
+    function setBoardWithFen() {
+        const newGame = new Chess(fenInput);
+        if (validateFen(fenInput)) {
+            setGame(newGame);
+        } else {
+            console.log("Invalid FEN");
+        }
     }
 
     return (
-        <div>
-            <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+        <div style={{ display: "flex", height: "100vh" }}>
+            <div style={{ flex: 1 }}>
+                <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+            </div>
+            <div style={{ flex: 1, padding: "10px" }}>
+                <div>
+                    <h3>Set Board</h3>
+                    <input
+                        type="text"
+                        value={fenInput}
+                        onChange={handleFenChange}
+                        placeholder="FEN"
+                    />
+                    <button onClick={setBoardWithFen}>Set Board</button>
+                </div>
+                <div>
+                    <h3>Evaluation</h3>
+                    <p>{getEval()}</p>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Engine.js
+++ src/Engine.js
@@ -1,16 +1,39 @@
 import { useState, useEffect, useCallback } from 'react';
 
-function useStockfishEngine() {
-    const [stockfish, setStockfish] = useState(null);
-    const [bestMove, setBestMove] = useState(null);
+// Wrapper around stockfish
+function Engine() {
+    const [stockfish, setStockfish] = useState();
+    const [fen, setFen] = useState();
+    const [data, setData] = useState();
+    const [bestMove, setBestMove] = useState();
+    const [evaluation, setEvaluation] = useState();
+
+    const handleMessage = (e) => {
+        setData(e["data"]);
+        // TODO parser would be nice but overkill
+        const bm_regex = /best move (\S+)/;
+        const eval_regex = /score cp (-?\d+)/;
+
+        const bm_match = e["data"].match(bm_regex);
+        const eval_match = e["data"].match(eval_regex);
+
+        if (bm_match) {
+            console.log("Found best move: ", bm_match);
+        }
+
+        if (eval_match) {
+            console.log("eval: ", eval_match[1]);
+        }
+
+        // console.log("[debug] e[data]: ", e["data"]);
+    }
+
+    const getEval = ()=> {
+        return evaluation;
+    }
 
     useEffect(() => {
         const stockfishWorker = new Worker("./stockfish.js");
-
-        const handleMessage = (e) => {
-            const move = e.data?.match(/bestmove\s+(\S+)/)?.[1];
-            setBestMove(move);
-        };
 
         stockfishWorker.addEventListener("message", handleMessage);
 
@@ -32,13 +55,13 @@ function useStockfishEngine() {
     const evaluatePosition = useCallback((fen, depth) => {
         sendMessage(`position fen ${fen}`);
         sendMessage(`go depth ${depth}`);
-    }, [sendMessage]);
+    }, [stockfish]);
 
     const stop = useCallback(() => {
         sendMessage("stop");
     }, [sendMessage]);
 
-    return { evaluatePosition, stop, bestMove };
+    return { evaluatePosition, getEval, stop };
 }
 
-export default useStockfishEngine;
+export default Engine;
